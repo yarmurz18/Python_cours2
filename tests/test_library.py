0560e0f316f3471dba8c6c07f2d681d8340fe0fe
yaroslav <yaroslavmurz@hmail.com>
2025-04-22 18:18:53 +0300

    clean

--- tests/test_library.py
+++
@@ -1,7 +0,0 @@
-class TestLibrary:
-    def test_new_library(self, library):
-        assert library.name
-        assert not library.book_list
-
-    def test_name_in_library(self, library, another_library):
-        assert library.name != another_library.name
